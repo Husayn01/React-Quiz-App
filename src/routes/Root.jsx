dde520e6603c98879edd9e34c799d9c1efb59e19
Husayn <hussainiahmed222@gmail.com>
2023-03-15 11:49:28 +0100

    Update Root.jsx

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -21,9 +21,9 @@ function Root() {
             <div className="attribution">
             <p>Developed by Husayn</p>
             <a href="https://www.github.com/Husayn01"><i class="fab fa-github"></i></a>
-            <a href=""><i class="fab fa-facebook"></i></a>
-            <a href=""><i class="fab fa-twitter"></i></a>
-            <a href=""><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://www.facebook.com/hussaini.ahmed.1293"><i class="fab fa-facebook"></i></a>
+            <a href="https://mobile.twitter.com/Husayn_01"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.linkedin.com/mwlite/in/hussaini-ahmed-a51232252"><i class="fab fa-linkedin-in"></i></a>
             </div>
         </motion.div>
       </div>
